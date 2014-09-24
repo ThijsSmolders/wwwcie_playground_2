79188a3baee1209322e4b50c1532ed3ebed91baf
ThijsSmolders <hahathijs@umad.nl>
2014-09-24 21:32:13 +0200

    django
    Ishetgelukt

--- msn/views.py
+++ msn/views.py
@@ -5,4 +5,9 @@ from msn.forms import BerichtForm
 def msn_chat(request):
     all_msg = Bericht.objects.all()
     form=BerichtForm(request.POST)
-    return render(request, 'msn_test.html', {'all_msg':all_msg})
+    if form.is_valid():
+        form.save()
+    return render(request, 'msn_test.html', {
+        'all_msg':all_msg,
+        'form': form,
+    })
