0ff7dbfea1f55104cbaefd8be49cb1346ff4d8ef
ThijsSmolders <hahathijs@umad.nl>
2014-09-24 21:10:22 +0200

    views_niet_forms
    JWZ

---
+++ msn/forms.py
@@ -0,0 +1,6 @@
+from django.forms import ModelForm
+from msn.models import Bericht
+
+class BerichtForm(ModelForm):
+    class Meta:
+        model=Bericht
--- msn/views.py
+++ msn/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from msn.models import Bericht
+from msn.forms import BerichtForm
 
 def msn_chat(request):
-    return render(request, 'msn_test.html')
+    all_msg = Bericht.objects.all()
+    form=BerichtForm(request.POST)
+    return render(request, 'msn_test.html', {'all_msg':all_msg})
