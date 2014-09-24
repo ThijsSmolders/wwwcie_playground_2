abd765b386dfbd6f37c8450780bd88b98d4619cf
Peterie <peter.callaars@gmail.com>
2014-09-24 20:13:22 +0200

    urls
    kan ik ook. hahahaha

--- wwwcie/urls.py
+++ wwwcie/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from msn.views import msn_chat
+from irc.views import irc
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^msn/', msn_chat),
+    url(r'^irc/', irc),
 )
