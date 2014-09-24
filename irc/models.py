817478f42e9913bc1b32eb44cae274e66c457238
Peterie <peter.callaars@gmail.com>
2014-09-24 20:15:00 +0200

    models
    =model

--- irc/models.py
+++ irc/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Memo():
+class Memo(models.Model):
     guest = models.ForeignKey(User)
     memo = models.TextField()
 # time = models.DateTimeField()
